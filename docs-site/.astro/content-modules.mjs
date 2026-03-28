04dee778f2a0751ed614552a174eff9197eae157
Aaron Sachs <898627+asachs01@users.noreply.github.com>
2026-03-28 11:28:51 -0400

    fix: default docs to dark mode, improve prose text contrast

--- docs-site/.astro/content-modules.mjs
+++ docs-site/.astro/content-modules.mjs
@@ -1,21 +1,21 @@
 
 export default new Map([
+["src/content/docs/chores.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fchores.mdx&astroContentModuleFlag=true")],
+["src/content/docs/calendar.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcalendar.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/configuration.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fconfiguration.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/api-reference.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi-reference.mdx&astroContentModuleFlag=true")],
-["src/content/docs/calendar.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcalendar.mdx&astroContentModuleFlag=true")],
-["src/content/docs/contributing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcontributing.mdx&astroContentModuleFlag=true")],
+["src/content/docs/family.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ffamily.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/dashboard.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdashboard.mdx&astroContentModuleFlag=true")],
-["src/content/docs/chores.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fchores.mdx&astroContentModuleFlag=true")],
+["src/content/docs/contributing.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcontributing.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/deployment.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdeployment.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/faq.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ffaq.mdx&astroContentModuleFlag=true")],
-["src/content/docs/family.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ffamily.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/getting-started.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/grocery.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgrocery.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/music.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmusic.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/notifications.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fnotifications.mdx&astroContentModuleFlag=true")],
+["src/content/docs/photos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fphotos.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/screensaver.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fscreensaver.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/themes.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fthemes.mdx&astroContentModuleFlag=true")],
-["src/content/docs/photos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fphotos.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/timer.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftimer.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/welcome.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fwelcome.mdx&astroContentModuleFlag=true")]]);
 		
